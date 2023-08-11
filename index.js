cde60d097d6efca8bebabdfcf5d669ea625c026c
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-11 09:22:44 -0400

    arrow functions

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (topNumber, bottomNumber) => topNumber / bottomNumber;
+
+const square = (multiply) => multiply * multiply;
+
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
